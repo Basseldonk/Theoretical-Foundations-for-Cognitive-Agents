3626c8671d15428f500603aea0750f915e6fc114
Bram van Asseldonk <Bram@2xp.nl>
2018-06-06 20:25:52 +0200

    Created folders to structure the project

--- scripts/Dataset_generator.py
+++ scripts/Dataset_generator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.6
 
+import numpy as np
 import cv2
 
 class dataset_generator:
@@ -9,6 +10,13 @@ class dataset_generator:
 
     def __init__(self):
         pass
+
+    def showImage(self, img):
+        img = cv2.imread(img, 1)
+        cv2.imshow("Image", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     dg = dataset_generator
