d8db01335c1ab89154d384ef4517c7f8406f24cb
Bram van Asseldonk <Bram@2xp.nl>
2018-06-06 20:00:52 +0200

    Added Dataset_generator.py

---
+++ Dataset_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.6
+
+import cv2
+
+class dataset_generator:
+    """
+    Shows users an image and lets them indicate whether they like this or not. Feedback is stored to generate a dataset.
+    """
+
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    dg = dataset_generator
