8743f4cc956213486c2e7b3a6b0d3d81473f77e8
Bram van Asseldonk <Bram@2xp.nl>
2018-06-13 22:29:24 +0200

    Script now saves your data after rating faces.

--- scripts/Dataset_generator.py
+++ scripts/Dataset_generator.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import KFold
 import Facial_Network as nn
 import numpy as np
 import random
+import pickle
 import cv2
 import os
 
@@ -13,12 +14,12 @@ class dataset_generator:
     Shows users an image and lets them indicate whether they like this or not. Feedback is stored to generate a dataset.
     """
 
-    def __init__(self):
+    def __init__(self, demo=False):
         self.dataset = []
         self.datapath = "../resources/cfd/CFD Version 2.0.3/CFD 2.0.3 Images/"
         sex = input("To which gender are you sexually attracted? (M/F)")
         print(sex)
-        self.rateData(sex)
+        self.rateData(sex, demo=demo)
 
     def accessPicture(self, target):
         """
@@ -49,7 +50,7 @@ class dataset_generator:
                 cv2.destroyAllWindows()
                 return False
 
-    def rateData(self, sex):
+    def rateData(self, sex, demo=False):
         """ 
         Shows image of preferred sex in random order and stores preference rating. 
         Arguments:
@@ -61,12 +62,18 @@ class dataset_generator:
         for dir in dirs:
             if dir[1] == sex.upper() and i < 50:
                 self.dataset.append((dir, self.showImage(self.accessPicture(dir))))
-                i += 1
+                if demo:  
+                    i += 1
+
 
 if __name__ == "__main__":
+    name = input("What is your name?")
+    
+    # Comment out the next part to skip the rating of faces and load an existing file.
     dg = dataset_generator()
-    print("Len of dataset = {}".format(len(dg.dataset)))
-    MLPinput, MLPlabels = nn.buildMLPtrainInput(dg.dataset)
+    pickle.dump(dg.dataset, open("../resources/saved data/saved_data_" + name + ".p", 'wb'))
+    
+    MLPinput, MLPlabels = nn.buildMLPtrainInput(pickle.load( open("../resources/saved data/saved_data_" + name + ".p", 'rb')))
     clf = MLPClassifier(solver='lbfgs', hidden_layer_sizes=(33), random_state=1, verbose = True)
     scores = cross_val_score(clf, MLPinput, MLPlabels, cv=10)
     print("Accuracy: %0.1f (+/- %0.1f)" % (scores.mean(), scores.std() * 2))
